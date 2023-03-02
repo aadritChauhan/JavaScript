5e34c08455e645938f4956e9b11563f02c4f905f
Aadrit Chauhan <114449390+aadritChauhan@users.noreply.github.com>
2023-03-02 13:05:18 -0500

    Add files via upload

---
+++ Customised UI/lab9.js
@@ -0,0 +1,34 @@
+﻿//LAB 9-DATA STORAGE: INDEX PAGE
+window.onload= pageLoaded();
+
+function pageLoaded(){
+
+    var formHandle= document.forms.infoForm;
+    formHandle.onsubmit= processForm;
+    
+    function processForm(){
+     var name= formHandle.f_name.value;
+    var favC= formHandle.f_color.value;
+    localStorage.setItem("Sname", name);
+    
+    localStorage.setItem("Scolor", favC);
+    }
+
+    var userName=localStorage.getItem("Sname");
+    var userC=localStorage.getItem("Scolor");
+
+    if(userName!=null){
+    newMsgBox.innerHTML="Welcome "+userName;
+     document.body.style.backgroundColor=userC;
+     }
+
+    document.getElementById("btnDel").onclick=()=>{
+        localStorage.removeItem("Sname");
+        localStorage.removeItem("Scolor");
+        window.location.reload();
+
+
+    }
+
+
+}
---
+++ Customised UI/products.js
@@ -0,0 +1,15 @@
+﻿//LAB 9-DATA STORAGE: PRODUCTS PAGE
+
+window.onload= pageLoaded();
+
+function pageLoaded(){
+
+    var userName=localStorage.getItem("Sname");
+    var userC=localStorage.getItem("Scolor");
+
+    if(userName!==null){
+        document.getElementById("newMsgBox").innerHTML="Welcome "+ userName;
+        document.body.style.backgroundColor= userC;
+        }
+
+}
